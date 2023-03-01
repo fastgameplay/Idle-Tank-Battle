2e5be699982ef5421e46b2e8ba26651e26742093
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2023-03-01 19:39:54 +0400

    Import tank files

---
+++ Assets/Scripts/Bullet/BulletMovement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    // Target location
+    // Destroy after Bullet
+    // send to bulletColision target
+    // destroy via object destroyer
+}
